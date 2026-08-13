7b7c7f79fb2efa367cb51482d44c737f4baaa8aa
Sudhanshu Sahu <sudhanshukumar40234@gmail.com>
2026-08-13 21:33:23 +0530

    Add binary search implementation in Binary-Search.c
    Implement a binary search algorithm to find an element in a sorted array.

---
+++ DAA/Binary-Search.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, key;
+    int low, high, mid;
+    int found = 0;
+
+    // Input size
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // Input sorted array
+    printf("Enter %d elements in sorted order:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Element to search
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    // Binary Search
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+
+        if (arr[mid] == key) {
+            printf("Element %d found at index %d.\n", key, mid);
+            found = 1;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
+}
