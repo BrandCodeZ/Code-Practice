764da5951c7682a55635042684bb497fe73c9217
Sudhanshu Sahu <sudhanshukumar40234@gmail.com>
2026-08-13 21:38:29 +0530

    Add binary search implementation using recursion in C

---
+++ DAA/Binary Search using Recursion in C..c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    // Base condition: element not found
+    if (low > high)
+        return -1;
+
+    int mid = low + (high - low) / 2;
+
+    // Element found
+    if (arr[mid] == key)
+        return mid;
+
+    // Search in the right half
+    if (arr[mid] < key)
+        return binarySearch(arr, mid + 1, high, key);
+
+    // Search in the left half
+    return binarySearch(arr, low, mid - 1, key);
+}
+
+int main()
+{
+    int arr[100], n, key, result;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements in sorted order:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, 0, n - 1, key);
+
+    if (result == -1)
+        printf("Element not found.\n");
+    else
+        printf("Element found at index %d.\n", result);
+
+    return 0;
+}
