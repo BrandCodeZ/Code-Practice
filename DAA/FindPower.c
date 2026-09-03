85f0b3c4bc9afa6d1294b7f36e2c62a05e618109
Sudhanshu Sahu <sudhanshukumar40234@gmail.com>
2026-09-03 15:33:15 +0530

    Find The Power OF elements using Divide and Conquer

---
+++ DAA/FindPower.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Function to calculate a^n using Divide and Conquer
+long long power(int a, int n)
+{
+    // Base case
+    if (n == 0)
+        return 1;
+
+    // Divide: calculate a^(n/2)
+    long long half = power(a, n / 2);
+
+    // Conquer and combine
+    if (n % 2 == 0)
+        return half * half;
+    else
+        return a * half * half;
+}
+
+int main()
+{
+    int a, n;
+
+    printf("Enter the base: ");
+    scanf("%d", &a);
+
+    printf("Enter the power: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("Please enter a non-negative power.\n");
+    }
+    else
+    {
+        printf("%d^%d = %lld\n", a, n, power(a, n));
+    }
+
+    return 0;
+}
