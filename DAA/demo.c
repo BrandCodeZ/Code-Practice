9e4bf5e885876878b1bf2fd3d9f7f2b2110fd341
Sudhanshu Sahu <sudhanshukumar40234@gmail.com>
2026-09-16 22:36:02 +0530

    Initial Commit after Pull after reposit

---
+++ DAA/demo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    float height;
+    char grade;
+    char name[20];
+
+    // Asking for input
+    printf("Enter your age, height, grade, and name: ");
+
+    // Reading input from the user
+    scanf("%d %f %c %s", &age, &height, &grade, name);
+
+    // Displaying the input using printf
+    printf("Your age: %d\n", age);
+    printf("Your height: %.2f\n", height);  // .2f for 2 decimal places
+    printf("Your grade: %c\n", grade);
+    printf("Your name: %s\n", name);
+
+    return 0;
+}
